7f79b25290de0fe5e61445e02de3d6af417b1e61
Okuyama Yuya <nogideca@gmail.com>
2016-11-04 02:50:36 +0900

    ok

--- DSL/Main.hs
+++ DSL/Main.hs
@@ -8,31 +8,33 @@ import Syntax
 -- import Evaluation
 import System.Environment (getArgs)
 
-ss = [ "b : Int = 1",
-       "p1 : Pair Int Int = (1,2)",
-       "f : Int->Int = \\a:Int.3",
-       "w : List Int = [1,2,3,4]", 
-       "f1 : Int -> (List Int) -> List Int = cons",
-       "sum : (List Int) -> Int = foldr plus 0",
-       "" ]
+-- ss = [ "b : Int = 1",
+--        "p1 : Pair Int Int = (1,2)",
+--        "f : Int->Int = \\a:Int.3",
+--        "w : List Int = [1,2,3,4]", 
+--        "f1 : Int -> (List Int) -> List Int = cons",
+--        "sum : (List Int) -> Int = foldr plus 0",
+--        "" ]
+
 main::IO()
 main = do
   s <- (head <$> getArgs) >>= readFile
   -- let s = unlines ss
-  print "lex"
-  print $ scanTokens $ s
-  putStr "\n"
+  -- print "lex"
+  -- print $ scanTokens $ s
+  -- putStr "\n"
   print "syntax"
-  print $ parse.scanTokens $ s
-  putStr "\n"
-  print "type"
+  let (Accept ( Program ls)) = parse.scanTokens $ s
+  mapM_ print ls
+  -- putStr "\n"
+  -- print "type"
   let res_p = parse.scanTokens $ s in
     case res_p of
      Reject err -> putStrLn $ show err
      _ -> let res_t = tycheck res_p in
           case res_t of
             Reject err -> putStrLn err
-            Accept a -> print a
+            Accept _ -> print "type check OK"
             -- True -> let res_e = eval res_p in putStrLn $ show res_e
 
 
