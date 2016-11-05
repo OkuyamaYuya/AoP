63613c5ddf0b0291439eeb2abac7756143e2d19c
Okuyama Yuya <nogideca@gmail.com>
2016-11-06 02:59:55 +0900

    add shell script.

---
+++ DSL/monotoneCheck.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FILE="./test.z3"
+
+if [ ! -f ${FILE} ]; then
+
+  echo "file not exists."
+
+else
+  result=`z3 -T:30 ${FILE}`
+
+  if [ "$result" = "unsat" ]; then
+    echo "ok"
+  else
+    echo not monotonic""
+  fi
+
+  if  echo "$result" | grep "error" >/dev/null 2>&1; then
+    echo "there's error."
+    echo $result
+  fi
+
+fi
+
