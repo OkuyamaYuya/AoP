047256b9bea986272e017c5d5793113521e0a457
Okuyama Yuya <nogideca@gmail.com>
2016-11-05 19:10:44 +0900

    reform

--- DSL/testKnapsack.gd
+++ DSL/testKnapsack.gd
@@ -4,7 +4,9 @@ BASETYPE : Pair Int Int
 LEFT     : [e1]
 RIGHT    : [f1,f2]
 
-pairPlus p1 p2 : (Pair Int Int)->(Pair Int Int)->(Pair Int Int) = (fst p1 + fst p2 , snd p1 + snd p2)
+pairPlus p1 p2 :
+  (Pair Int Int)->(Pair Int Int)->(Pair Int Int) =
+  (fst p1 + fst p2 , snd p1 + snd p2)
 
 pairSum : (List (Pair Int Int))->(Pair Int Int) = foldr pairPlus (0,0)
 
@@ -16,11 +18,13 @@ w : Int = 10
 
 p x : (List (Pair Int Int))->Bool = leq (sumWt x) w
 
-r a b : (List (Pair Int Int))->(List (Pair Int Int))->Bool =
-                                        leq (sumVal a) (sumVal b)
+r a b :
+  (List (Pair Int Int))->(List (Pair Int Int))->Bool =
+  leq (sumVal a) (sumVal b)
 
-q a b : (List (Pair Int Int))->(List (Pair Int Int))->Bool =
-                (leq (sumVal a) (sumVal b)) && ((sumWt a) == (sumWt b))
+q a b :
+  (List (Pair Int Int))->(List (Pair Int Int))->Bool =
+  (leq (sumVal a) (sumVal b)) && ((sumWt a) == (sumWt b))
 
 e1 : (List (Pair Int Int)) = nil
 f1 : (Pair Int Int) -> (List (Pair Int Int)) -> (List (Pair Int Int)) = cons
