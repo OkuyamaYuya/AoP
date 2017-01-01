e9e9c2b44b0f2bf8640986d2d84b11e5d6a25cec
Okuyama Yuya <nogideca@gmail.com>
2017-01-02 01:43:10 +0900

    a

---
+++ DSL/example/test2Knapsack.gd
@@ -0,0 +1,33 @@
+-- 0-1 knapsack problem
+
+TYPE : (Int,Int)
+BASE : [e1]
+STEP : [f1,f2]
+INSTANCE : [ (50,4),(3,12),(1,1),(10,5),(4,31),(4,2)]
+
+
+pairPlus p1 p2 :
+  (Int,Int)->(Int,Int)->(Int,Int) =
+  (fst p1 + fst p2 , snd p1 + snd p2)
+
+pairSum : (List (Int,Int))->(Int,Int) = foldr pairPlus (0,0)
+
+sumVal x : (List (Int,Int))->Int = fst (pairSum x)
+
+sumWt x : (List (Int,Int))->Int = snd (pairSum x)
+
+w : Int = 10
+
+ff (x,y) : (Int,Int) -> Int = 1
+
+p x : (List (Int,Int))->Bool = (sumWt x) <= w
+
+r a b :
+  (List (Int,Int))->(List (Int,Int))->Bool = sumVal a <= sumVal b
+
+q a b :
+  (List (Int,Int))->(List (Int,Int))->Bool = (sumVal a <= sumVal b) && (sumWt a == sumWt b)
+
+e1 : (Int,Int) = (0,0)
+f1 x : ((Int,Int),(Int,Int)) -> (Int,Int) = ( fst (fst x) + fst (snd x) , snd (fst x) + snd (snd x) )
+f2 : ((Int,Int),(Int,Int)) -> (Int,Int) = snd
