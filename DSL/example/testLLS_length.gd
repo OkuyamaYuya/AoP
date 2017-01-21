79f3a2947f5eb149ddc3cbf2a5d5abd01727ad6a
Okuyama Yuya <nogideca@gmail.com>
2017-01-21 17:37:32 +0900

    push

--- DSL/example/testLLS_length.gd
+++ DSL/example/testLLS_length.gd
@@ -4,13 +4,13 @@ ITYPE : Int
 OTYPE : List Int
 BASE  : [nil]
 STEP : [cons,outr]
-INSTANCE : [5,3,-3,3,34,5,7,2,3,55,65,4,-1,1,2]
+INSTANCE : [5,3,-3,3,74,5,7,2,3,55,4,-1,1,2]
 
-plus1 a as : Int -> (List Int) -> Int = 1
-length : (List Int)->Int = foldr plus1 0
+plus1 x y : Int -> Int -> Int = 1 + y
+len : (List Int)->Int = foldr plus1 0
 
 r : (List Int)->(List Int)->Bool = leq_lexico
-q : (List Int)->(List Int)->Bool = leq_lexico
+q a b : (List Int)->(List Int)->Bool = leq_lexico a b && len a == len b
 
-p x : (List Int) -> Bool = length x <= 3
+p x : (List Int) -> Bool = len x <= 3
 
